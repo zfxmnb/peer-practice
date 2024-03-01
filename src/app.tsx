efca33a287fc4d9ddcb255770354daa28f9a791e
zengfanxing <zengfanxing@joyy.com>
2024-03-01 13:58:38 +0800

    feat: 支持url传入自定义服务配置

--- src/app.tsx
+++ src/app.tsx
@@ -263,6 +263,9 @@ export function App() {
     host && (options.host = host);
     port && (options.port = Number(port));
     path && (options.path = path);
+    if (options.port && options.port !== 443) {
+      options.secure = false;
+    }
     peer = new Peer(myInfo?.id, options);
     peer.on('open', (id: string) => {
       const myInfo = myInfoRef.current ?? { id, nickName: getNickName() };
